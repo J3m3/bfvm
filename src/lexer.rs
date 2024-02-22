f882b78d2404796c0fab971b2db84c6decd59c12
YJS <y.j3ms.n@gmail.com>
2024-02-22 18:43:45 +0900

    test: add tests for lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -30,3 +30,88 @@ impl<'a> Lexer<'a> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_lex_single_operations() {
+        let ops = vec!['+', '-', '<', '>', ',', '.', '[', ']'];
+        let expected = vec![
+            OpKind::Inc,
+            OpKind::Dec,
+            OpKind::Left,
+            OpKind::Right,
+            OpKind::Input,
+            OpKind::Output,
+            OpKind::Jeq0Forward,
+            OpKind::Jne0Backward,
+        ];
+
+        for (op, exp) in ops.into_iter().zip(expected.into_iter()) {
+            let input = op.to_string();
+            let mut lexer = Lexer::new(&input);
+            assert_eq!(lexer.next(), Some(exp));
+            assert_eq!(lexer.next(), None);
+        }
+    }
+
+    #[test]
+    fn should_ignore_invalid_characters() {
+        let mut lexer = Lexer::new("a*b&c");
+        assert_eq!(lexer.next(), None);
+    }
+
+    #[test]
+    fn should_lex_sequences_of_operations() {
+        let mut lexer = Lexer::new("+-<>[],.");
+        let expected = vec![
+            OpKind::Inc,
+            OpKind::Dec,
+            OpKind::Left,
+            OpKind::Right,
+            OpKind::Jeq0Forward,
+            OpKind::Jne0Backward,
+            OpKind::Input,
+            OpKind::Output,
+        ];
+
+        for e in expected {
+            assert_eq!(lexer.next(), Some(e));
+        }
+        assert_eq!(lexer.next(), None);
+    }
+
+    #[test]
+    fn should_lex_sequences_of_operations_with_non_bf() {
+        let mut lexer = Lexer::new("HI+I'M-NOT< A >VALID[BF],OP.");
+        let expected = vec![
+            OpKind::Inc,
+            OpKind::Dec,
+            OpKind::Left,
+            OpKind::Right,
+            OpKind::Jeq0Forward,
+            OpKind::Jne0Backward,
+            OpKind::Input,
+            OpKind::Output,
+        ];
+
+        for e in expected {
+            assert_eq!(lexer.next(), Some(e));
+        }
+        assert_eq!(lexer.next(), None);
+    }
+
+    #[test]
+    fn should_lex_empty_input() {
+        let mut lexer = Lexer::new("");
+        assert_eq!(lexer.next(), None);
+    }
+
+    #[test]
+    fn should_ignore_input_with_only_invalid_characters() {
+        let mut lexer = Lexer::new("abcde");
+        assert_eq!(lexer.next(), None);
+    }
+}
